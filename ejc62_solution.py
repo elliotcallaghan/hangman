340867fc3d1e6229e353e1bca3663d7467691872
elliotcallaghan <callaghan.elliot@yahoo.com>
2018-11-14 18:30:29 +0000

    Finished game stats function and added validation for letter input

--- ejc62_solution.py
+++ ejc62_solution.py
@@ -1,3 +1,9 @@
+import random
+
+num_games = 1
+wins = 0
+losses = 0
+
 #compares letters_guessed to secret_word
 def is_secret_guessed(secret_word, letters_guessed):
     for letter in range(len(secret_word)):
@@ -19,23 +25,33 @@ def get_current_guess(secret_word, letters_guessed):
     return current_guess
 
 #loops through the first game
-def first_game(secret_word = "Emmanuel"):
-    secret_word = secret_word.lower()
+def first_game(secret_word):
+    global wins
+    global losses
+    secret_word = secret_word.strip().lower()
     letters_guessed = []
-    guesses_remaining = 5
+    guesses_remaining = 10
     previous_guess = "_" * len(secret_word)
 
     print("The secret word has " + str(len(secret_word)) + " characters.")
     print("You have " + str(guesses_remaining) + " guesses.")
 
     while guesses_remaining > 0:
-        letters_guessed.append(input("Guess a character in the secret word: ").lower())
+        while True:
+            letter = input("Guess a character in the secret word: ").lower()
+            if letter.isalpha() and len(letter) == 1:
+                break
+            else:
+                print("Input must be one letter")
+
+        letters_guessed.append(letter)
         current_guess = get_current_guess(secret_word, letters_guessed)
         print("The partially guessed word is", current_guess)
 
         if is_secret_guessed(secret_word, letters_guessed) == True:
             print("It's a win!")
-            break
+            wins += 1
+            guesses_remaining = 0
         else:
             if current_guess == previous_guess:
                 guesses_remaining -= 1
@@ -44,6 +60,9 @@ def first_game(secret_word = "Emmanuel"):
         previous_guess = current_guess
     else:
         print("The word was", secret_word)
+        if secret_word != previous_guess:
+            losses += 1
+        
 
 #loads file and puts words into list
 def load_words(file_name):
@@ -57,13 +76,27 @@ def load_words(file_name):
 
 #chooses a random word from list
 def choose_secret_word():
-    import random
     words = load_words("words.txt")
     return words[0][random.randint(0, words[1] - 1)]
 
 #loads first game but with random word
 def second_game():
-    first_game(choose_secret_word())
+    global num_games
+    quit = input("Press c to continue or q to quit: ").lower()
+    if (quit == "q"):
+        stats = game_stats(num_games)
+        print("Wins: " + str(stats[0]) + "\nLosses: " + str(stats[1]) + "\n" + stats[2])
+    elif (quit == "c"):
+        num_games += 1
+        first_game(choose_secret_word())
+        second_game()
+    else:
+        second_game()
 
-first_game()
+#takes in number of games and returns win/loss frequencies and average
+def game_stats(num_games):
+    average = "The average win percentage is " + str(float(wins / num_games) * 100) + "%"
+    return (wins, losses, average)
+
+first_game("Emmanuel")
 second_game()
